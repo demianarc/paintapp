6bb9e45a7f21d33b329afdf041eb8b4808185e15
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-04 23:36:42 +0200

    gpt4fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
 import openai
+import requests
 import random
 import os
 import json
@@ -11,7 +12,7 @@ import json
 app = Flask(__name__)
 
 # Load OpenAI API key from environment variables
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+gpt4_api_key = os.environ.get("GPT4_API_KEY")
 
 def scrape_painting():
     api_key = os.environ.get("HARVARD_API_KEY")
@@ -44,25 +45,64 @@ def scrape_painting():
         "date": date
     }
 
+def generate_chat_completion(messages, model="gpt-4", temperature=1, max_tokens=None):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {GPT4_API_KEY}",
+    }
+
+    data = {
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+    }
+
+    if max_tokens is not None:
+        data["max_tokens"] = max_tokens
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        raise Exception(f"Error {response.status_code}: {response.text}")
+
 def generate_artwork_info(artist, title):
     prompts = [
-        f"You are an art critic and poet, make a deep interpretation of '{title}' by {artist}. Write 3 words this artwork inspires you. Make it short if possible in bullet points. Include a short section that explains how it could resonate with our current society.",
-        f"Describe the emotions that '{title}' by {artist} evokes in you. Write a short paragraph about how this artwork connects with the viewer on an emotional level.",
-        f"Provide a random fact or interesting detail about '{title}' by {artist} or similar artworks. Explain how this fact contributes to the overall understanding of the piece.",
+        # ...
     ]
 
     prompt = random.choice(prompts)
 
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    # Call the GPT-4 API
+    def gpt4_request(prompt, max_tokens=150, temperature=0.7):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {gpt4_api_key}",
+        }
+        data = {
+            "model": "gpt-4",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+        }
 
-    return response.choices[0].text.strip()
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers=headers,
+            data=json.dumps(data),
+        )
+
+        if response.status_code == 200:
+            return response.json()["choices"][0]["message"]["content"].strip()
+        else:
+            raise Exception(f"Error {response.status_code}: {response.text}")
+
+    response_text = gpt4_request(prompt)
+    return response_text
 
 
 @app.route('/')
@@ -70,7 +110,7 @@ def painting_of_the_day():
     painting = scrape_painting()
     painting_info = generate_artwork_info(painting["artist"], painting["title"])
     painting["info"] = painting_info
-    return render_template('index.html', painting=painting)
+    return render_template("index.html", painting=painting)
 
 if __name__ == '__main__':
     import os
