a7e9b17c0aded746746f78ea449800c2fc33e46b
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-06 15:53:27 +0200

    rollback5

--- app.py
+++ app.py
@@ -53,25 +53,16 @@ def generate_artwork_info(artist, title):
 
     prompt = random.choice(prompts)
 
-    response = openai.ChatCompletion.create(
-    model="gpt-4",  # Use GPT-4 instead of text-davinci-002
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a helpful art critic and poet. Provide deep interpretations and insights about artworks.",
-        },
-        {
-            "role": "user",
-            "content": f"Interpret '{title}' by {artist}. Write 3 words this artwork inspires you. Make it short if possible in bullet points. Include a short section that explains how it could resonate with our current society.",
-        },
-    ],
-    max_tokens=150,
-    n=1,
-    stop=None,
-    temperature=0.7,
-)
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
 
-    return response['choices'][0]['message']['content'].strip()
+    return response.choices[0].text.strip()
 
 
 @app.route('/')
