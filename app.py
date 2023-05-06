fc64c11873310cac8f325e274952156eee48b56b
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-06 17:15:43 +0200

    test11

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 # Load OpenAI API key from environment variables
 openai.api_key = os.environ.get("OPENAI_API_KEY")
+API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
 def scrape_painting():
     api_key = os.environ.get("HARVARD_API_KEY")
@@ -45,24 +46,28 @@ def scrape_painting():
     }
 
 def generate_artwork_info(artist, title):
-    prompts = [
-        f"You are an art critic and poet, make a deep interpretation of '{title}' by {artist}. Write 3 words this artwork inspires you. Make it short if possible in bullet points. Include a short section that explains how it could resonate with our current society.",
-        f"Describe the emotions that '{title}' by {artist} evokes in you. Write a short paragraph about how this artwork connects with the viewer on an emotional level.",
-        f"Provide a random fact or interesting detail about '{title}' by {artist} or similar artworks. Explain how this fact contributes to the overall understanding of the piece.",
-    ]
+    payload = {
+        "model": "gpt-4",
+        "messages": [
+            {"role": "system", "content": f"Please provide a deep interpretation of '{title}' by {artist}. Write 3 words that this artwork inspires you. Make it short if possible in bullet points. Include a short section that explains how it could resonate with our current society."},
+            {"role": "assistant", "content": "Sure, I can help with that. Let me generate some ideas for you."},
+            {"role": "user", "content": ""}
+        ],
+        "temperature": 0.5,
+        "max_tokens": 256,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+    }
 
-    prompt = random.choice(prompts)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai.api_key}"
+    }
 
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    response = requests.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers)
 
-    return response.choices[0].text.strip()
+    return response.json()["choices"][0]["text"].strip()
 
 
 @app.route('/')
