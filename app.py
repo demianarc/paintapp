9a6ac84bf48f72c29dc6eebbd813f7fc552af52f
Dylan B <dylan@MacBook-Pro-D-2.local>
2023-05-10 13:33:55 +0200

    newrefresh9

--- app.py
+++ app.py
@@ -55,16 +55,23 @@ def generate_artwork_info(artist, title):
 
     prompt = random.choice(prompts)
 
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
+    response = openai.ChatCompletion.create(
+        model="gpt-4",  # Use the appropriate chat model
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
         max_tokens=150,
-        n=1,
-        stop=None,
         temperature=0.7,
     )
 
-    return response.choices[0].text.strip()
+    return response['choices'][0]['message']['content'].strip()
 
 
 @app.route('/')
